de2dac545188ddfa3bcf426130004b6a75a54b3a
ninhpham <ninhpm@mz.co.kr>
2024-10-15 09:57:45 +0700

    update default language vi

--- geonode/settings.py
+++ geonode/settings.py
@@ -773,6 +773,7 @@ MIDDLEWARE = (
     'django_user_agents.middleware.UserAgentMiddleware',
     'geonode.base.middleware.MaintenanceMiddleware',
     'geonode.base.middleware.ReadOnlyMiddleware',   # a Middleware enabling Read Only mode of Geonode
+    'setmydefaultlanguage.ForceDefaultLanguageMiddleware',
 )
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
